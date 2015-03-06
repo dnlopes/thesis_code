bac3c6ece6975af4993ecb277d632ded86d3cbbb
David Lopes <david.anlopes@gmail.com>
2015-03-06 16:55:36 +0000

    added source

---
+++ src/util/Defaults.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ * Created by dnlopes on 05/03/15.
+ */
+public class Defaults
+{
+	public static final String MYSQL_USER = "sa";
+	public static final String MYSQL_PASSWORD = "101010";
+	public static final int MYSQL_PORT = 53306;
+	public static final String CRDT_URL_PREFIX = "jdbc:crdt://";
+	public static final String DEFAULT_URL_PREFIX = "jdbc:mysql://";
+}
