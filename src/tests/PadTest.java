9b18135ed41b7aadfb9f3e684d633c25a276d982
David Lopes <david.anlopes@gmail.com>
2015-03-20 01:52:59 +0000

    padtest modified

--- src/tests/PadTest.java
+++ src/tests/PadTest.java
@@ -9,6 +9,7 @@ import org.perf4j.LoggingStopWatch;
 import org.perf4j.StopWatch;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -33,8 +34,52 @@ public class PadTest
 		watch.stop();
 
 		Connection con = ConnectionFactory.getInstance().getCRDTConnection(Configuration.DB_NAME);
-
 		Statement stat = con.createStatement();
-		stat.executeUpdate("update address set addr_state='ola' where addr_state='cenas';");
+
+		/*
+		mysql> select * from t1;
+		+---+----+----+----+-----------------+---------+--------+-----------+-----------+
+		| a | b  | c  | d  | e               | _SP_del | _SP_ts | _SP_clock | _SP_immut |
+		+---+----+----+----+-----------------+---------+--------+-----------+-----------+
+		| 0 | 10 | 10 |  3 | qqxoKbLIGfBhdSR |         |      0 | 0-0       |         0 |
+		| 1 |  9 |  1 | 10 | gkSJnjFGFbvhFrm |         |      0 | 0-0       |         1 |
+		| 2 |  6 |  7 | 10 | LWMEOmhiVRmIPSR |         |      0 | 0-0       |         2 |
+		| 3 |  6 |  6 |  8 | RDmngbBXYTstMyx |         |      0 | 0-0       |         3 |
+		| 4 |  2 |  2 |  1 | dSptOXtkJnybbzF |         |      0 | 0-0       |         4 |
+		| 5 |  1 |  2 |  2 | kJTVHegDlHDVpwq |         |      0 | 0-0       |         5 |
+		| 6 |  7 |  2 |  3 | pSsbDbvxRPvcBja |         |      0 | 0-0       |         6 |
+		| 7 |  1 |  2 |  2 | FQpJTHpHIdurAgk |         |      0 | 0-0       |         7 |
+		| 8 |  3 |  9 |  5 | xFUZcuEbwzXWGtq |         |      0 | 0-0       |         8 |
+		| 9 |  6 |  9 |  5 | YzIjgKtkHlxHUjG |         |      0 | 0-0       |         9 |
+		+---+----+----+----+-----------------+---------+--------+-----------+-----------+
+		*/
+
+
+		// expected: 4
+		int result = stat.executeUpdate("update t1 set b=10 where d=10 OR c=9");
+
+		// expected: 5
+		result = stat.executeUpdate("delete from t1 where b=10");
+
+
+		//expected: 2
+		ResultSet rs = stat.executeQuery("SELECT * from t1 where d=2");
+		result = countResultSetRows(rs);
+
+
+		// expected: 3
+		rs = stat.executeQuery("SELECT * from t1 where d=2 OR b=3");
+		result = countResultSetRows(rs);
+
+		//int res = stat.executeUpdate("insert into t1 (a,b,c,d,e) values(54,6,1,1,ZZZZ)");
+
+	}
+
+	private static int countResultSetRows(ResultSet rs) throws SQLException
+	{
+		int i = 0;
+		while(rs.next())
+			i++;
+		return i;
 	}
 }
